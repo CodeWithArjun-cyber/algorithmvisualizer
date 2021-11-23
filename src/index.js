9cecb09d1c9ae99aec214077ced3174f576a5c50
Arjun <arjunprajapati3786@gmail.com>
2021-11-23 11:59:42 +0530

    adding index.css file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { ModalProvider } from "react-modal-hook";
 import App from "./pages/App";
-
+import './index.css'
 ReactDOM.render(
   <ModalProvider>
     <App />
